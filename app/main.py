b018fa2ff4799f8ec256338d7b10408ea71d3774
isaac weathers <isaac@isaacweathers.net>
2025-01-26 03:25:28 +0000

    updating requirements

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.routers import articles
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize FastAPI app
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Include routers (endpoints)
 app.include_router(articles.router)
